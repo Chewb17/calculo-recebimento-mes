854858f698ac87c155340245f3158f4b3d07bcb6
chewb17 <lucas.moraes@estudante.ufjf.br>
2025-05-07 13:51:12 -0300

    Initial commit

--- sales_project/sales/urls.py
+++ sales_project/sales/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CustomAuthToken, SaleListView, RegisterView, SaleDetailView
+from .views import CustomAuthToken, SaleListView, RegisterView
 
 urlpatterns = [
     path('login/', CustomAuthToken.as_view(), name='api_login'),
     path('sales/', SaleListView.as_view(), name='api_sales'),
-    path('sales/<int:pk>/', SaleDetailView.as_view(), name='api_sales_detail'),  # Corrigir para usar SaleDetailView
+    path('sales/<int:pk>/', SaleListView.as_view(), name='sale-detail'),
     path('register/', RegisterView.as_view(), name='api_register'),
 ]
